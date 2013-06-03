0962342bbc042e5329375f3042272b5c4f331b4e
Rodrigo <ccagnas@MacBook-Pro-de-Cesar.local>
2013-06-03 15:57:18 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SopaEnSobre::Application.config.secret_token = 'a79d721e7a093c1f9a0b07d17a34f46c76d55617fc1cc384883fa0a5bac4e9e0ecf79212ca613e529fc85383ac899fe9434084e9cb2e1ea5720df1996edb815a'
